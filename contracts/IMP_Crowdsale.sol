6e23f40bb8d52007e645c32070a45759a7debe97
Ivan Solomichev <vanijatko.social@gmail.com>
2018-06-07 11:33:13 +0300

    Token will be initialized with parameters for token usage only. Crowdsale contract will be initialized with crowdsale parameters

--- contracts/IMP_Crowdsale.sol
+++ contracts/IMP_Crowdsale.sol
@@ -7,23 +7,63 @@ import "../node_modules/openzeppelin-solidity/contracts/crowdsale/validation/Whi
 
 contract IMP_Crowdsale is WhitelistedCrowdsale {
 
-  IMP_Token public token;
+  enum CrowdsaleType {preICO, ico}
+  enum MintPurpose {pre_ICO, ico, team, platform, airdrops} // Supplier.State.inactive
 
-  uint256 icoTotalSupplyLimit;
+  IMP_Token public token;
+  CrowdsaleType public crowdsaleType;
+
+  uint256 public tokenPercentageReserved_preICO;  //  % of tokens reserved for pre_ICO
+  uint8 public tokenPercentageReserved_ico;       //  % of tokens reserved for ICO
+  uint8 public tokenPercentageReserved_team;      //  % of tokens reserved for team
+  uint8 public tokenPercentageReserved_platform;  //  % of tokens reserved for platform 
+  uint8 public tokenPercentageReserved_airdrops;  //  % of tokens reserved for airdrops
+
+  uint256 public tokenLimitTotalSupply;        //  tokens total supply for crowdsale
+  uint256 public tokenLimitReserved_preICO;    //  tokens reserved for pre_ICO
+  uint256 public tokenLimitReserved_ico;       //  tokens reserved for ICO
+  uint256 public tokenLimitReserved_team;      //  tokens reserved for team
+  uint256 public tokenLimitReserved_platform;  //  tokens reserved for platform 
+  uint256 public tokenLimitReserved_airdrops;  //  tokens reserved for airdrops
+
+  uint256 public tokensMinted_preICO;    //  tokens minted for pre_ICO
+  uint256 public tokensMinted_ico;       //  tokens minted for ICO
+  uint256 public tokensMinted_team;      //  tokens minted for team
+  uint256 public tokensMinted_platform;  //  tokens minted for platform 
+  uint256 public tokensMinted_airdrops;  //  tokens minted for airdrops
 
   /**
    * MODIFIERS
    */
 
+
+  /**
+   * PUBLIC
+   */
+
+
   /**
    * @dev Constructor function.
    * @param _rate Token amount per one Eth
-   * @param _icoTotalSupplyLimit Value in wei involved in the purchase
    */
-  constructor(uint256 _rate, uint256 _icoTotalSupplyLimit, uint256 _tokenPercentagePurchaseLimit, address _wallet, IMP_Token _token) Crowdsale(_rate, _wallet, _token) public {
+  constructor(CrowdsaleType _crowdsaleType, uint256 _rate, address _wallet, IMP_Token _token, uint256 _tokenLimitTotalSupply, uint8 _tokenPercentageReserved_preICO, uint8 _tokenPercentageReserved_ico, uint8 _tokenPercentageReserved_team, uint8 _tokenPercentageReserved_platform, uint8 _tokenPercentageReserved_airdrops) Crowdsale(_rate, _wallet, _token) public {
+    crowdsaleType = _crowdsaleType;
     token = IMP_Token(_token);
-    icoTotalSupplyLimit = _icoTotalSupplyLimit;
+
+    tokenPercentageReserved_preICO = _tokenPercentageReserved_preICO;
+    tokenPercentageReserved_ico = _tokenPercentageReserved_ico;
+    tokenPercentageReserved_team = _tokenPercentageReserved_team;
+    tokenPercentageReserved_platform = _tokenPercentageReserved_platform;
+    tokenPercentageReserved_airdrops = _tokenPercentageReserved_airdrops;
+
+    tokenLimitTotalSupply = _tokenLimitTotalSupply;
+    tokenLimitReserved_preICO = tokenLimitTotalSupply * _tokenPercentageReserved_preICO;
+    tokenLimitReserved_ico = tokenLimitTotalSupply * _tokenPercentageReserved_ico;
+    tokenLimitReserved_team = tokenLimitTotalSupply * _tokenPercentageReserved_team;
+    tokenLimitReserved_platform = tokenLimitTotalSupply * _tokenPercentageReserved_platform;
+    tokenLimitReserved_airdrops = tokenLimitTotalSupply * _tokenPercentageReserved_airdrops;
   }
+
 
   /**
    * OVERRIDEN
@@ -35,15 +75,47 @@ contract IMP_Crowdsale is WhitelistedCrowdsale {
    * @param _weiAmount Value in wei involved in the purchase
    */
   function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
+    validatePurchaceLimit(_weiAmount, crowdsaleType);
+
     super._preValidatePurchase(_beneficiary, _weiAmount);
   }
    
-   /**
-    * @dev Source of tokens. Override this method to modify the way in which the crowdsale ultimately gets and sends its tokens.
-    * @param _beneficiary Address performing the token purchase
-    * @param _tokenAmount Number of tokens to be emitted
-    */
+  /**
+   * @dev Source of tokens. Override this method to modify the way in which the crowdsale ultimately gets and sends its tokens.
+   * @param _beneficiary Address performing the token purchase
+   * @param _tokenAmount Number of tokens to be emitted
+   */
   function _deliverTokens(address _beneficiary, uint256 _tokenAmount) internal {
     token.mint(_beneficiary, _tokenAmount);
   }
+
+  /**
+   * @dev Override to extend the way in which ether is converted to tokens.
+   * @param _weiAmount Value in wei to be converted into tokens
+   * @return Number of tokens that can be purchased with the specified _weiAmount
+   */
+  function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
+    return _weiAmount.mul(rate);
+  }
+
+  /**
+   * PRIVATE
+   */
+
+  /**
+   * @dev Validation of crowdsale limits.
+   * @param _weiAmount Value in wei to be calculated
+   * @param _crowdsaleType Type of current crowdsale
+   */
+   function validatePurchaceLimit(uint256 _weiAmount, CrowdsaleType _crowdsaleType) private view {
+      uint256 pendingTokens = _getTokenAmount(_weiAmount);
+
+     if(_crowdsaleType == CrowdsaleType.preICO) {
+       require(tokensMinted_preICO.add(pendingTokens) <= tokenLimitReserved_preICO);
+     } else if(_crowdsaleType == CrowdsaleType.ico) {
+       require(tokensMinted_ico.add(pendingTokens) <= tokenLimitReserved_ico);
+     }
+
+     revert();
+   }
 }
--- migrations/2_initial_migration.js
+++ migrations/2_initial_migration.js
@@ -10,15 +10,20 @@ module.exports = (deployer, network, accounts) => {
     const TOKEN_SYMBOL = "IMP";
     const TOKEN_DECIMALS = 4;
 
-    const ICO_RATE = 10; // tokens per 1 ETH
-    const ICO_WALLET = accounts[4];
-    const ICO_TOTAL_SUPPLY_LIMIT = 100000000 * 10000; //  100 000 000 * 4 decimals
-    const ICO_TOKEN_PERCENTAGE = 30; //  30% - pre-ICO, 42% - ICO, 18% - team, 10% - platform to start
+    const CROWDSALE_RATE = 10; // tokens per 1 ETH
+    const CROWDSALE_WALLET = accounts[4];
+    const CROWDSALE_TOTAL_SUPPLY_LIMIT = 100000000; //  100 000 000
+
+    const TOKEN_PERCENTAGE_RESERVED_PRE_ICO = 30;
+    const TOKEN_PERCENTAGE_RESERVED_ICO = 44;
+    const TOKEN_PERCENTAGE_RESERVED_TEAM = 18;
+    const TOKEN_PERCENTAGE_RESERVED_PLATFORM = 5;
+    const TOKEN_PERCENTAGE_RESERVED_AIRDROPS = 2;
 
     deployer.deploy(IMP_Token, TOKEN_NAME, TOKEN_SYMBOL, TOKEN_DECIMALS).then(async () => {
         let token = await IMP_Token.deployed();
 
-        await deployer.deploy(IMP_Crowdsale, ICO_RATE, ICO_TOTAL_SUPPLY_LIMIT, ICO_TOKEN_PERCENTAGE, ICO_WALLET, token.address);
+        await deployer.deploy(IMP_Crowdsale, 0, CROWDSALE_RATE, CROWDSALE_WALLET, token.address, CROWDSALE_TOTAL_SUPPLY_LIMIT, TOKEN_PERCENTAGE_RESERVED_PRE_ICO, TOKEN_PERCENTAGE_RESERVED_ICO, TOKEN_PERCENTAGE_RESERVED_TEAM, TOKEN_PERCENTAGE_RESERVED_PLATFORM, TOKEN_PERCENTAGE_RESERVED_AIRDROPS);
         let crowdsale = await IMP_Crowdsale.deployed();
 
         await token.transferOwnership(crowdsale.address);
