fef3db213bceae8fa8bbf69d0f68f4405f1e5f9c
Ivan Solomichev <vanijatko.social@gmail.com>
2018-06-20 18:00:00 +0300

    Created Shared Ledger contract and moved percents and reserved numbers into it.

--- migrations/2_initial_migration.js
+++ migrations/2_initial_migration.js
@@ -1,5 +1,6 @@
 let IMP_Token = artifacts.require("./IMP_Token.sol");
 let IMP_Crowdsale = artifacts.require("./IMP_Crowdsale.sol");
+let IMP_CrowdsaleSharedLedger = artifacts.require("IMP_CrowdsaleSharedLedger");
 let IncreaseTime = require("../test/helpers/increaseTime.js");
 
 /**
@@ -11,8 +12,6 @@ module.exports = (deployer, network, accounts) => {
     const TOKEN_SYMBOL = "IMP";
     const TOKEN_DECIMALS = 4;
 
-    const MINIMUM_PURCHASE_WEI = web3.toWei(0.00001, "ether"); //  TODO: calculate proper value depending on rate
-    const CROWDSALE_TYPE_PRE_ICO = 0;
     const CROWDSALE_WALLET = accounts[4];
     const CROWDSALE_TOTAL_SUPPLY_LIMIT = 100000000; //  no decimals
     const CROWDSALE_OPENING = web3.eth.getBlock("latest").timestamp + IncreaseTime.duration.minutes(1);
@@ -29,7 +28,11 @@ module.exports = (deployer, network, accounts) => {
 
     deployer.deploy(IMP_Token, TOKEN_NAME, TOKEN_SYMBOL, TOKEN_DECIMALS).then(async () => {
         let token = await IMP_Token.deployed();
-        await deployer.deploy(IMP_Crowdsale, CROWDSALE_TYPE_PRE_ICO, [CROWDSALE_OPENING, CROWDSALE_CLOSING], CROWDSALE_WALLET, token.address, CROWDSALE_TOTAL_SUPPLY_LIMIT, [TOKEN_PERCENTAGE_RESERVED_PRE_ICO, TOKEN_PERCENTAGE_RESERVED_ICO, TOKEN_PERCENTAGE_RESERVED_TEAM, TOKEN_PERCENTAGE_RESERVED_PLATFORM, TOKEN_PERCENTAGE_RESERVED_AIRDROPS], PRE_ICO_DISCOUNTS);
+
+        await deployer.deploy(IMP_CrowdsaleSharedLedger, token.address, CROWDSALE_TOTAL_SUPPLY_LIMIT, [TOKEN_PERCENTAGE_RESERVED_PRE_ICO, TOKEN_PERCENTAGE_RESERVED_ICO, TOKEN_PERCENTAGE_RESERVED_TEAM, TOKEN_PERCENTAGE_RESERVED_PLATFORM, TOKEN_PERCENTAGE_RESERVED_AIRDROPS]);
+        let sharedLedger = await IMP_CrowdsaleSharedLedger.deployed();
+
+        await deployer.deploy(IMP_Crowdsale, token.address, sharedLedger.address, CROWDSALE_WALLET, [CROWDSALE_OPENING, CROWDSALE_CLOSING]);
         let crowdsale = await IMP_Crowdsale.deployed();
 
         await token.transferOwnership(crowdsale.address);
