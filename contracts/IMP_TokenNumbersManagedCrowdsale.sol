e67922accc2786152cd72b8317fc22e10ecf7ea4
Ivan Solomichev <vanijatko.social@gmail.com>
2018-06-22 19:23:20 +0300

    Added medthod to check current crowdsale type

--- contracts/IMP_TokenNumbersManagedCrowdsale.sol
+++ contracts/IMP_TokenNumbersManagedCrowdsale.sol
@@ -40,6 +40,10 @@ contract IMP_TokenNumbersManagedCrowdsale is Ownable {
         getTokenReservedLimits();
   }
 
+  function currentCrowdsaleType() public view returns(IMP_CrowdsaleSharedLedger.CrowdsaleType) {
+    return crowdsaleSharedLedger.crowdsaleType();
+  }
+
   /**
    * @dev Calculate available amount of tokens to mint during purchase.
    * @return Number of tokens that can be minted during purchase
