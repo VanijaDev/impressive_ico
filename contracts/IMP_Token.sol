4ba4152f32c49d5a0ea2d19ac77942c13ae6273e
Ivan Solomichev <vanijatko.social@gmail.com>
2018-08-30 19:47:53 +0300

    Token details updated

--- contracts/IMP_Token.sol
+++ contracts/IMP_Token.sol
@@ -7,6 +7,6 @@ import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/DetailedERC2
 contract IMP_Token is MintableToken, DetailedERC20 {
   using SafeMath for uint256;
 
-  constructor() DetailedERC20("Impressive Token", "IMP", 4) public {
+  constructor() DetailedERC20("Impresso", "XIM", 4) public {
   }
 }
