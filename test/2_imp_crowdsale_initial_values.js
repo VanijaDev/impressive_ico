c865f04a71e7eee44fc89d978cca7de730aedc82
Ivan Solomichev <vanijatko.social@gmail.com>
2018-08-31 18:41:30 +0300

    renamed before tests

---
+++ test/2_imp_crowdsale_initial_values.js
@@ -0,0 +1,7 @@
+contract('impCrowdsaleInitialValues', function(accounts) {
+  it("should assert true", function(done) {
+    var imp_crowdsale_initial_values = impCrowdsaleInitialValues.deployed();
+    assert.isTrue(true);
+    done();
+  });
+});
