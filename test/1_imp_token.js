a6769d1344c74a6b8a456f93d814abad5e92c4ba
Ivan Solomichev <vanijatko.social@gmail.com>
2018-09-01 20:13:20 +0300

    token description tests

--- test/1_imp_token.js
+++ test/1_imp_token.js
@@ -1,21 +1,21 @@
-let IMP_Token = artifacts.require("./IMP_Token.sol");
+// let IMP_Token = artifacts.require("./IMP_Token.sol");
 
-contract("IMP_Token", (accounts) => {
-  let token;
+// contract("IMP_Token", (accounts) => {
+//   let token;
 
-  before("setup", async () => {
-    token = await IMP_Token.deployed();
-  });
+//   before("setup", async () => {
+//     token = await IMP_Token.deployed();
+//   });
 
-  it("should validate token name after migration", async () => {
-    assert.equal(await token.name.call(), "Impresso", "wrong token name");
-  });
+//   it("should validate token name after migration", async () => {
+//     assert.equal(await token.name.call(), "Impresso", "wrong token name");
+//   });
 
-  it("should validate token symbol after migration", async () => {
-    assert.equal(await token.symbol.call(), "XIM", "wrong token symbol");
-  });
+//   it("should validate token symbol after migration", async () => {
+//     assert.equal(await token.symbol.call(), "XIM", "wrong token symbol");
+//   });
 
-  it("should validate token decimals after migration", async () => {
-    assert.equal(await token.decimals.call(), 4, "wrong token decimals");
-  });
-});
+//   it("should validate token decimals after migration", async () => {
+//     assert.equal(await token.decimals.call(), 4, "wrong token decimals");
+//   });
+// });
