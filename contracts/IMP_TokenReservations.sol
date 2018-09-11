3d293f590f88898249756318c3115add0c8707b1
Ivan Solomichev <vanijatko.social@gmail.com>
2018-09-11 09:33:41 +0300

    empty line removed

--- contracts/IMP_TokenReservations.sol
+++ contracts/IMP_TokenReservations.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.24;
 
 import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
-
 import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract IMP_TokenReservations is Ownable {
